cbf4de5eacf49f11d86cd450872ff864019efc58
okaryu569 <okaryu569@gmail.com>
2016-06-22 19:12:26 +0900

    Add answers

--- src/main/java/benkyokai/Main.java
+++ src/main/java/benkyokai/Main.java
@@ -2,7 +2,10 @@ package benkyokai;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -22,7 +25,7 @@ public class Main {
 	Student student5 = new Student("川西", "2015002", Arrays.asList("Math", "English", "Chemistry", "Pysics"));			
 	student5.setPerformances(Pair.of("Math", 75), Pair.of("English", 55), Pair.of("Chemistry", 60), Pair.of("Pysics", 75));
 	
-	//List<Student> students 学生のリスト
+	//List<Student> students 学生のデータのリスト
 	//String 名前、String 学籍番号（入学年度+個人番号）、List<String> 履修科目、Map<String, Integer> 履修科目と点数
 	//名前、学籍番号、科目と点数
 	//奥田、2016001、(数学、70)(英語、70)
@@ -39,45 +42,54 @@ public class Main {
 		System.out.println(student.toString());
 	}
 	//forEach を使って書いてみる　(匿名メソッド)
-//	students.forEach(new Consumer<Student>(){
-//			... //実装するメソッド名は accept, 戻り値なし, 引数は Student 型)
-//	});
+	students.forEach(new Consumer<Student>(){
+		@Override
+		public void accept(Student student) {
+			System.out.println(student.toString());
+
+		}
+	});
 	//forEach を使って書いてみる　(ラムダ式)
-//	students.forEach(student -> 
-//	...
+	students.forEach(student -> System.out.println(student.toString()));
 
 	System.out.println("02, 英語の点数が80点以上の生徒はいますか？");
 	boolean exists;
-//	exists = students.stream()
-//			.anyMatch(student -> {
-//			...
-//	System.out.println(exists);
+	exists = students.stream()
+			.anyMatch(student -> student.getPerformances().get("English") >= 80 );			
+	System.out.println(exists);
 	
 	System.out.println("03, 数学の最高点をとったのは誰ですか？");
 	Student st;
-//	st = students.stream()
-//			.collect(Collectors.maxBy((st1, st2) -> {
-//				...
-//	System.out.println(st);
+	st = students.stream()
+			.collect(Collectors.maxBy((student, st2) -> student.getPerformances().get("Math") - st2.getPerformances().get("Math")))
+			.get();		
+	System.out.println(st);
 	
 	System.out.println("04, 科目情報を取り除き、名前と学籍番号だけのリストを作ってください");
 	List<Pair<String, String>> list01 = new ArrayList<>();
-//	list01 = students.stream()
-//			.map(student -> {
-//				...			
-//	list01.forEach(pair -> System.out.println(pair.toString()));
+	list01 = students.stream()
+			.map(student -> Pair.of(student.getName(), student.getNo()))
+			.collect(Collectors.toList());			
+	list01.forEach(pair -> System.out.println(pair.toString()));
 	
 	System.out.println("05, 2016年入学で,数学の点数順に並べられた,生徒の名前のリストを作ってください");
 	List<String> list02 = new ArrayList<>();
-//	list02 = students.stream()
-//			...
-//	list03.forEach(name -> System.out.println(name));	
+	list02 = students.stream()
+			.filter(student -> student.getNo().startsWith("2016"))
+			.sorted(Comparator.comparing(student -> student.getPerformances().get("Math")))
+			.map(student -> student.getName())
+			.collect(Collectors.toList());			
+	list02.forEach(name -> System.out.println(name));	
 
 	System.out.println("EX, 3科目以上受講している人の名前と平均点のリストを作ってください");
 	List<Pair<String, Double>> listEX = new ArrayList<>(); //余裕のある人は List<Pair<String, Double>> ではなく Map<String, Double> にしてみましょう
-//	listEX = students.stream()
-//			...
-//	listEX.forEach(pair -> System.out.println(pair.toString()));
+	listEX = students.stream()
+			.filter(student -> student.getLessons().size() >= 3)
+			.map(student -> Pair.of(student.getName(), 
+					student.getPerformances().values().stream().collect(Collectors.averagingDouble(p -> p))))
+			.collect(Collectors.toList());
+			
+	listEX.forEach(pair -> System.out.println(pair.toString()));
     }
 }
 
